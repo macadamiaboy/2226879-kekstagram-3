5d501cfc7fb6469933cd913f79421ff3a5d1fcf2
macadamiaboy <elknumber1@yandex.com>
2023-03-24 01:46:27 +0300

    Even more bugs fixed

--- js/data.js
+++ js/data.js
@@ -6,15 +6,13 @@ const PHOTOS_AMOUNT = 25;
 
 const list = [];
 
-const newPhoto = (index) => {
-  return  {
-    id: index,
-    url: `photos/${index}.jpg`,
-    description: 'It\'s me at Dubai and I\'m on my vacation right now',
-    likes: customRandom(15, 200),
-    comments: customRandom(0, 200),
-  };
-};
+const newPhoto = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: 'It\'s me at Dubai and I\'m on my vacation right now',
+  likes: customRandom(15, 200),
+  comments: customRandom(0, 200),
+});
 
 const fillTheList = () => {
   for (let i = 0; i < PHOTOS_AMOUNT; i++) {
